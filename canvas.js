b8bd8d90612823c73d3a8653ba20bffd3700aa93
Ryan <ryan.j.van.es@gmail.com>
2019-04-08 21:21:18 +0200

    canvas part 3

--- canvas.js
+++ canvas.js
@@ -1,34 +1,39 @@
 var canvas = document.getElementById('canvas');
-var run = true;
-var obstacleX = 0;
-var obstacleY = 0;
-var playerX = 0;
-var playerY = 0;
-if (canvas.getContext) {
-	var ctx = canvas.getContext('2d');
-	//window
-	ctx.fillStyle = "rgb(0, 0, 0)";
-	ctx.fillRect(50, 50, 125, 125);
-	ctx.clearRect(63, 63, 100, 100);
-	//coloring the window
-	ctx.fillStyle = "rgb(243, 101, 35)";
-	ctx.fillRect(63, 63, 45, 45);
-	ctx.fillStyle = "rgb(141, 199, 64)";
-	ctx.fillRect(118, 63, 45, 45);
-	ctx.fillStyle = "rgb(0, 174, 240)";
-	ctx.fillRect(63, 118, 45, 45);
-	ctx.fillStyle = "rgb(255, 194, 15)";
-	ctx.fillRect(118, 118, 45, 45);
-	ctx.fillStyle = "rgb(0, 0, 0)";
-	ctx.fillRect(63, 108, 100, 10);
-	ctx.fillRect(108, 63, 10, 100);
-	//lines surrounding window
+var ctx = canvas.getContext("2d");
+var x = 200;
+var y = 200;
+var xVel = 6;
+var yVel = 3;
+var radius = 30;
+canvas.width = window.innerWidth-30;
+canvas.height = window.innerHeight-30;
+function animated() {
+	requestAnimationFrame(animated);
+	ctx.clearRect(0,0,canvas.width,canvas.height);
 	ctx.beginPath();
-	ctx.moveTo(25,25);
-	ctx.lineTo(200,25);
-	ctx.lineTo(200,200);
-	ctx.lineTo(25,200);
-	ctx.lineTo(25, 25);
-	ctx.strokeStyle = "rgb(0,0,0)";
+	ctx.arc(x, y, 30,0,Math.PI*2, true);
+	ctx.strokeStyle = "purple";
 	ctx.stroke();
+	ctx.fillStyle = "purple";
+	ctx.fill();
+	checkBoundaries();
+	x+=xVel;
+	y+=yVel
+}
+function checkBoundaries(){
+	if (x + radius + xVel > canvas.width) {
+		xVel *= -1;
+	}
+	if (x - radius + xVel < 0) {
+		xVel *= -1;
+	}
+	if (y + radius + yVel > canvas.height) {
+		yVel *= -1;
+	}
+	if (y - radius + yVel < 0) {
+		yVel *= -1;
+	}
+}
+if (canvas.getContext) {
+	animated();
 }
