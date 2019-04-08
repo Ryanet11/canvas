322fef303cd82e586b118f4d8f0577e970e7b70d
Ryan <ryan.j.van.es@gmail.com>
2019-04-08 18:27:39 +0200

    canvas leren stap 2

--- canvas.js
+++ canvas.js
@@ -6,17 +6,29 @@ var playerX = 0;
 var playerY = 0;
 if (canvas.getContext) {
 	var ctx = canvas.getContext('2d');
-	ctx.an
-	ctx.fillStyle = "red";
-	ctx.fillRect(117, 120, 10, 80);
-	ctx.fillRect(213, 120, 10, 80);
-	ctx.fillRect(117, 216, 10, 80);
-	ctx.fillRect(213, 216, 10, 80);	
-	ctx.fillRect(130, 107, 80, 10);
-	ctx.fillRect(130, 203, 80, 10);
-	ctx.fillRect(130, 299, 80, 10);
+	//window
+	ctx.fillStyle = "rgb(0, 0, 0)";
+	ctx.fillRect(50, 50, 125, 125);
+	ctx.clearRect(63, 63, 100, 100);
+	//coloring the window
+	ctx.fillStyle = "rgb(243, 101, 35)";
+	ctx.fillRect(63, 63, 45, 45);
+	ctx.fillStyle = "rgb(141, 199, 64)";
+	ctx.fillRect(118, 63, 45, 45);
+	ctx.fillStyle = "rgb(0, 174, 240)";
+	ctx.fillRect(63, 118, 45, 45);
+	ctx.fillStyle = "rgb(255, 194, 15)";
+	ctx.fillRect(118, 118, 45, 45);
+	ctx.fillStyle = "rgb(0, 0, 0)";
+	ctx.fillRect(63, 108, 100, 10);
+	ctx.fillRect(108, 63, 10, 100);
+	//lines surrounding window
 	ctx.beginPath();
-	ctx.moveTo(80, 200);
-	ctx.lineTo(50, 400);
+	ctx.moveTo(25,25);
+	ctx.lineTo(200,25);
+	ctx.lineTo(200,200);
+	ctx.lineTo(25,200);
+	ctx.lineTo(25, 25);
+	ctx.strokeStyle = "rgb(0,0,0)";
 	ctx.stroke();
 }
